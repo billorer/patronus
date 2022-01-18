12fe5399a65f43bda6e5bb328b27c6f2643059ce
sutoz <zoran@suto.ro>
2022-01-18 22:19:53 +0100

    Made emergency-list component to lazy load Modified readme Moved some components Added rawdata model interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,16 @@
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EmergencyListComponent } from './emergency-list/emergency-list.component';
-
-import { TableModule } from 'primeng/table';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 import { AuthInterceptor } from 'src/interceptors/auth.interceptor';
 import { HttpErrorInterceptor } from 'src/interceptors/error.interceptor';
-import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, EmergencyListComponent, NotFoundComponent],
-  imports: [BrowserModule, AppRoutingModule, TableModule, HttpClientModule],
+  declarations: [AppComponent, NotFoundComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
